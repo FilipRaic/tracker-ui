964776390bbdd177df376051d55afd479f1317e9
AdrianVucko <adrian.viktorija@gmail.com>
2025-06-22 18:37:34 +0200

    HT-18: Fixed tests

--- src/app/general-overview/general-overview.component.spec.ts
+++ src/app/general-overview/general-overview.component.spec.ts
@@ -1,16 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GeneralOverviewComponent } from './general-overview.component';
+import {instance, mock, when} from 'ts-mockito';
+import {TipService} from '../service/wellbeing-tip.service';
+import {AchievementService} from '../service/achievement.service';
+import {TranslateTestingModule} from 'ngx-translate-testing';
+import {of} from 'rxjs';
 
 describe('GeneralOverviewComponent', () => {
   let component: GeneralOverviewComponent;
   let fixture: ComponentFixture<GeneralOverviewComponent>;
+  let tipServiceMock: TipService = mock(TipService);
+  let achievementServiceMock: AchievementService = mock(AchievementService);
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      imports: [GeneralOverviewComponent]
+      imports: [GeneralOverviewComponent,
+        TranslateTestingModule.withTranslations({ en: require('../../../src/assets/i18n/en.json')})],
+      providers: [
+        {provide: TipService, useValue: instance(tipServiceMock)},
+        {provide: AchievementService, useValue: instance(achievementServiceMock)}
+      ]
     })
     .compileComponents();
+    when(tipServiceMock.getTips()).thenReturn(of([]))
+    when(achievementServiceMock.getAchievements()).thenReturn(of([]))
 
     fixture = TestBed.createComponent(GeneralOverviewComponent);
     component = fixture.componentInstance;
